e6a9a4c9202b525902bf5424ebc3a4d869669cd9
Samuel Martel <leumas3003@gmail.com>
2021-06-05 20:00:54 +0100

    [Samu] new proyect Beer Catalogue

---
+++ src/main/java/com/sms/beercatalogue/BeerCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.sms.beercatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerCatalogueApplication.class, args);
+	}
+
+}
